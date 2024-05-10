2f1ab388eebe886fa64678d46ef4b25120aff8f6
WANG-HAO-REN <30613087+WANG-HAO-REN@users.noreply.github.com>
2024-05-10 11:30:36 +0800

    Update Lab2-code1.py
    新增註解&移除 split 句號後 的 ""

--- Lab2-code1.py
+++ Lab2-code1.py
@@ -1,24 +1,26 @@
-import os 
+import os
 file_path = r"c:\Users\Jimmy\Desktop\Recommender_Systems\UN201301.txt"
-d = {"data": "" ,"hotlines": 0, "sentences": 0 }
 data=dict()
 hotlines=dict()
 sentences=0
 with open(file_path, mode="r", encoding="utf-8") as f :
-    for line in f.readlines():
-        line = line.split(",")
-        if line[1] not in data.keys():
-            data[line[1]] = 1
-        else:
-            data[line[1]] = data[line[1]]+1
-        if line[2] not in hotlines.keys():
-            hotlines[line[2]] = line[2]
-        if "。" in line[9]:
-            l = line[9].split("。")
-            sentences = sentences + len(l)
-#sorted_data => list  dict(sorted_data) => dict 
+   for line in f.readlines():
+       line = line.split(",")
+       #計算日期次數
+       if line[1] not in data.keys():
+           data[line[1]] = 1
+       else:
+           data[line[1]] = data[line[1]]+1
+       #計算熱線條數
+       if line[2] not in hotlines.keys():
+           hotlines[line[2]] = line[2]
+       #計算 。 次數
+       if "。" in line[9]:
+           split_line = line[9].split("。")
+           new_line =[x for x in split_line if x.strip()!='']
+           sentences = sentences + len(new_line)
+#sorted_data => list  dict(sorted_data) => dict
 sorted_data = sorted(data.items(), key=lambda x:x[1], reverse=True)
-
 print( "Q1: Which date has the most news stories? {ans} ".format(ans=sorted_data[0][0]) )
 print( "Q2: How many distinct hotlines? {ans} ".format(ans=len(hotlines)) )
 print( "Q3: How many sentences (separated by a period) in all news documents?  {ans} ".format(ans=sentences) )
